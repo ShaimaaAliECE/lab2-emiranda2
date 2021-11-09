517fb6954d5243eded7c7ba64b26bd22cae74413
YourName <you@example.com>
2021-11-09 18:02:54 -0500

    finished connect 4 game

--- connect-four/src/App.js
+++ connect-four/src/App.js
@@ -1,25 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GridCell from './GridCell';
 
-function App() {
+class App extends Component{
+    render(){
+      const cells = [];
+
+      //Enclose all of each row into a container
+      for (let y = 5; y>= 0; y--){
+        const row = [];
+        for (let x = 0; y<7; x++){
+          cells.push(<GridCell key = {x} x={x} y={y}/>);
+        }
+        cells.push(<div key={y} className="row">{row}</div>)
+      }
+      
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1 className="App-title">Welcome to React</h1>
+        </header>
+      {cells}
+      </div>
   );
+}
 }
 
 export default App;
---
+++ connect-four/src/GridCell.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from  'react-redux';
+import { dropTile } from './actions'
+class GridCell extends Component{
+handleClick(){
+    //console.log(`Clicked on column ${this.props.x}`)
+
+    this.props.sendTileDrop(this.props.x)
+}
+
+  render(){
+    const board = this.props.board;
+    const x = this.props.x;
+    const y = this.props.y;
+
+    if (board[x][y] !== undefined){
+        if(board[x][y]==='red'){
+            classes += 'p2';
+        }else{
+            classes += 'p1';
+        }
+    }
+
+      return(
+<div className ={classes} onClick={() => this.handleClick()}>
+    <p>{this.props.x}, {this.props.y}</p>
+</div>
+
+      );
+  }
+}
+
+const stateToProps = state => {
+return {
+    board: state.board,
+};
+};
+
+const dispatchToProps = dispatch => {
+return{
+sendTileDrop: col => dispatch(dropTile(col))
+};
+};
+
+
+export default connect(stateToProps, dispatchToProps)(GridCell);
